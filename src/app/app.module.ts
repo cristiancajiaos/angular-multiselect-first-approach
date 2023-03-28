08a2144941d32558448c110d087aae0eb51ca33c
Cristian Cajiao <cristiancajiaos@outlook.com>
2023-03-28 09:55:53 -0300

    Creado layout con select del paquete funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,14 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton: true
+    }),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown/multiselect.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  public dropdownList = [];
+  public selectedItems = [];
+  public dropdownSettings: IDropdownSettings = {};
+
+  constructor(
+    private toastrService: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.dropdownList = [
+      { itemId: 1, itemText: 'Mumbai' },
+      { itemId: 2, itemText: 'Bangaluru' },
+      { itemId: 3, itemText: 'Pune' },
+      { itemId: 4, itemText: 'Navsari' },
+      { itemId: 5, itemText: 'New Delhi'}
+    ];
+
+    this.selectedItems = [
+      { itemId: 3, itemText: 'Pune' },
+      { itemId: 4, itemText: 'Navsari'}
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'itemId',
+      textField: 'itemText',
+      selectAllText: 'Select All',
+      unSelectAllText: 'Unselect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+  public onItemSelect(item: any): void {
+    this.toastrService.success(`Item Value of ${item.itemId}: ${item.itemText}`);
+  }
+
+  public onSelectAll(item: any): void {
+    this.toastrService.success(`Selected All Items`);
   }
 
 }
